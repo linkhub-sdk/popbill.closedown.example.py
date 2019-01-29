bc3047ba3ca8e5b90892f6cd7e2220a7b11f0715
kimhyunjin <khjzzm@gmail.com>
2019-01-29 14:38:27 +0900

    개별항목 출력

--- checkCorpNum.py
+++ checkCorpNum.py
@@ -25,21 +25,17 @@ try:
     CorpNum = testValue.testCorpNum
 
     # 조회할 사업자번호
-    targetCorpNum = "1234567891"
+    targetCorpNum = "1234567890"
 
     corpState = closedownService.checkCorpNum(CorpNum, targetCorpNum)
 
     print("=" * 15 + " 휴폐업조회 - 단건 " + "=" * 15)
-    print("state (휴폐업상태) : None-미확인, 0-등록되지 않은 사업자번호, 1-사업중, 2-폐업, 3-휴업")
-    print("corpNum (조회 사업자번호)")
-    print("typeDate (과세유형 전환일자)")
-    print("stateDate (휴폐업 일자)")
-    print("type (사업자 과세유형) : None-미확인, 1-일반과세자, 2-면세과세자, 3-간이과세자, 4-비영리법인, 국가기관")
-    print("checkDate (국세청 최종 확인일자)\n")
-
-    for key, value in corpState.__dict__.items():
-        if not key.startswith("__"):
-            print("%s : %s" % (key, value))
+    print("corpNum (조회 사업자번호) : %s " % corpState.corpNum)
+    print("type (사업자 과세유형 [None-미확인, 1-일반과세자, 2-면세과세자, 3-간이과세자, 4-비영리법인, 국가기관)] : %s " % corpState.type)
+    print("typeDate (과세유형 전환일자) : %s " % corpState.typeDate)
+    print("state (휴폐업상태 [None-미확인, 0-등록되지 않은 사업자번호, 1-사업중, 2-폐업, 3-휴업)] : %s " % corpState.state)
+    print("stateDate (휴폐업 일자) : %s " % corpState.stateDate)
+    print("checkDate (국세청 최종 확인일자) : %s " % corpState.checkDate)
 
 except PopbillException as PE:
     print("Exception Occur : [%d] %s" % (PE.code, PE.message))
--- checkCorpNums.py
+++ checkCorpNums.py
@@ -2,9 +2,12 @@
 # code for console Encoding difference. Dont' mind on it
 import sys
 import imp
+
 imp.reload(sys)
-try: sys.setdefaultencoding('UTF8')
-except Exception as E: pass
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
 
 import testValue
 
@@ -29,18 +32,13 @@ try:
     CorpStateList = closedownService.checkCorpNums(CorpNum, targetCorpNumList)
 
     print("=" * 15 + " 휴폐업조회 - 대량 " + "=" * 15)
-    print("corpNum (조회 사업자번호)")
-    print("state (휴폐업상태) : None-미확인, 0-등록되지 않은 사업자번호, 1-사업중, 2-폐업, 3-휴업")
-    print("typeDate (과세유형 전환일자)")
-    print("stateDate (휴폐업 일자)")
-    print("type (사업자 과세유형) : None-미확인, 1-일반과세자, 2-면세과세자, 3-간이과세자, 4-비영리법인, 국가기관")
-    print("checkDate (국세청 최종 확인일자)\n")
-
     for info in CorpStateList:
-        print("corpNum : %s" % info.corpNum)
-        for key, value in info.__dict__.items():
-            if not key.startswith("__"):
-                print("     %s : %s" % (key,value))
+        print("corpNum (조회 사업자번호) : %s " % info.corpNum)
+        print("type (사업자 과세유형 [None-미확인, 1-일반과세자, 2-면세과세자, 3-간이과세자, 4-비영리법인, 국가기관]) : %s " % info.type)
+        print("typeDate (과세유형 전환일자) : %s " % info.typeDate)
+        print("state (휴폐업상태 [None-미확인, 0-등록되지 않은 사업자번호, 1-사업중, 2-폐업, 3-휴업]) : %s " % info.state)
+        print("stateDate (휴폐업일자) : %s " % info.stateDate)
+        print("checkDate (국세청 최종 확인일자) : %s " % info.checkDate) + '\n'
 
 except PopbillException as PE:
-    print("Exception Occur : [%d] %s" % (PE.code , PE.message))
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
