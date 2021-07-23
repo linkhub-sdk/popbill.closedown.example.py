1a47bf3e8c5dc0c07b2893ea8dc7f710a17c8b18
john <john@linkhub.co.kr>
2021-07-23 13:54:52 +0900

    added CloseDown TaxType

--- checkCorpNum.py
+++ checkCorpNum.py
@@ -29,15 +29,15 @@ try:
     CorpNum = testValue.testCorpNum
 
     # 조회할 사업자번호
-    targetCorpNum = "1234567890"
+    targetCorpNum = "6798700433"
 
     corpState = closedownService.checkCorpNum(CorpNum, targetCorpNum)
 
     print("=" * 15 + " 휴폐업조회 - 단건 " + "=" * 15)
-    print("type(사업자 과세유형) [None-미확인, 1-일반과세자, 2-면세과세자, 3-간이과세자, 4-비영리법인, 국가기관]")
+    print("type(사업자 과세유형) [None-미확인, 10-일반과세자, 20-면세과세자, 30-간이과세자, 31-간이과세자(세금계산서 발급사업자), 40-비영리법인, 국가기관]")
     print("state(휴폐업상태) [None-미확인, 0-등록되지 않은 사업자번호, 1-사업중, 2-폐업, 3-휴업]\n")
     print("corpNum (조회 사업자번호) : %s " % corpState.corpNum)
-    print("type (사업자 과세유형) : %s " % corpState.type)
+    print("taxType (사업자 과세유형) : %s " % corpState.taxType)
     print("typeDate (과세유형 전환일자) : %s " % corpState.typeDate)
     print("state (휴폐업상태) : %s " % corpState.state)
     print("stateDate (휴폐업 일자) : %s " % corpState.stateDate)
