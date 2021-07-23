073c12f2008cf546b6d91adcb8386d2a5be16ae4
john <john@linkhub.co.kr>
2021-07-23 14:09:00 +0900

    fixed comment typo

--- checkCorpNum.py
+++ checkCorpNum.py
@@ -34,8 +34,9 @@ try:
     corpState = closedownService.checkCorpNum(CorpNum, targetCorpNum)
 
     print("=" * 15 + " 휴폐업조회 - 단건 " + "=" * 15)
-    print("type(사업자 과세유형) [None-미확인, 10-일반과세자, 20-면세과세자, 30-간이과세자, 31-간이과세자(세금계산서 발급사업자), 40-비영리법인, 국가기관]")
+    print("taxType(사업자 과세유형) [None-미확인, 10-일반과세자, 20-면세과세자, 30-간이과세자, 31-간이과세자(세금계산서 발급사업자), 40-비영리법인, 국가기관]")
     print("state(휴폐업상태) [None-미확인, 0-등록되지 않은 사업자번호, 1-사업중, 2-폐업, 3-휴업]\n")
+    
     print("corpNum (조회 사업자번호) : %s " % corpState.corpNum)
     print("taxType (사업자 과세유형) : %s " % corpState.taxType)
     print("typeDate (과세유형 전환일자) : %s " % corpState.typeDate)
