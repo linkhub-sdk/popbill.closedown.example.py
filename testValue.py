2f94bcb814db6144cc04c5e9a52f42fc35f927bb
정우석 <wsj@linkhubcorp.com>
2024-02-27 09:23:38 +0900

    주석(bool 타입) 오타 수정

--- testValue.py
+++ testValue.py
@@ -12,10 +12,10 @@ Python 연동 튜토리얼 안내 : https://developers.popbill.com/guide/closedo
     - LinkID : 링크허브에서 발급한 링크아이디
     - SecretKey : 링크허브에서 발급한 비밀키
 2) SDK 환경설정 옵션 설정
-    - IsTest : 연동환경 설정, true-테스트, false-운영(Production), (기본값:true)
-    - IPRestrictOnOff : 인증토큰 IP 검증 설정, true-사용, false-미사용, (기본값:true)
-    - UseStaticIP : 통신 IP 고정, true-사용, false-미사용, (기본값:false)
-    - UseLocalTimeYN : 로컬시스템 시간 사용여부, true-사용, false-미사용, (기본값:true)
+    - IsTest : 연동환경 설정, True-테스트, False-운영(Production), (기본값:True)
+    - IPRestrictOnOff : 인증토큰 IP 검증 설정, True-사용, False-미사용, (기본값:True)
+    - UseStaticIP : 통신 IP 고정, True-사용, False-미사용, (기본값:False)
+    - UseLocalTimeYN : 로컬시스템 시간 사용여부, True-사용, False-미사용, (기본값:True)
 """
 
 
@@ -25,16 +25,16 @@ LinkID = "TESTER"
 # 비밀키
 SecretKey = "SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I="
 
-# 연동환경 설정, true-테스트, false-운영(Production), (기본값:true)
+# 연동환경 설정, True-테스트, False-운영(Production), (기본값:True)
 IsTest = True
 
-# 인증토큰 IP 검증 설정, true-사용, false-미사용, (기본값:true)
+# 인증토큰 IP 검증 설정, True-사용, False-미사용, (기본값:True)
 IPRestrictOnOff = True
 
-# 통신 IP 고정, true-사용, false-미사용, (기본값:false)
+# 통신 IP 고정, True-사용, False-미사용, (기본값:False)
 UseStaticIP = False
 
-# 로컬시스템 시간 사용여부, true-사용, false-미사용, (기본값:true)
+# 로컬시스템 시간 사용여부, True-사용, False-미사용, (기본값:True)
 UseLocalTimeYN = True
 
 # 팝빌회원 사업자번호
